31695a3a56dc5aab75349ca3a8a537668796cf38
manimegalai271 <manimegalaik2710@gmail.com>
2026-01-29 15:22:19 +0530

    Fix frontend deployment config

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Container, Row, Col } from "react-bootstrap";
-import pic1 from '../assets/pic1.jpg'
-import pic2 from '../assets/pic2.jpg'
-import pic3 from '../assets/pic3.jpg'
-import pic4 from '../assets/pic4.jpg'
-import pic5 from '../assets/pic5.jpg'
-import pic6 from '../assets/pic6.jpg'
+import pic1 from '../assets/pic1.png'
+import pic2 from '../assets/pic2.png'
+import pic3 from '../assets/pic3.png'
+import pic4 from '../assets/pic4.png'
+import pic5 from '../assets/pic5.png'
+import pic6 from '../assets/pic6.png'
 import Particle from '../components/Particle';
 import About from '../components/Home/About';
 import Type from '../components/Home/Type';
